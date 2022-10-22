4a2173479db74b8c71855642373c9bbe4192deb2
ShifmanLior <38461249+ShifmanLior@users.noreply.github.com>
2022-10-22 13:20:28 +0300

    Create Randomlinewithgradient.js

---
+++ Randomlinewithgradient.js
@@ -0,0 +1,66 @@
+function setup() {
+  createCanvas(400, 400);
+  frameRate(3);
+
+}
+
+function draw() {
+
+
+  background(255);
+  strokeWeight(0);
+  translate (200 ,100);
+  
+  
+
+  let rectalheight = 8
+  let random1 = random(20, 300);
+  let random2 = random(20, 300);
+  let random3 = random(20, 300);
+  let random4 = random(20, 300);
+  let random5 = random(20, 300);
+  let random6 = random(20, 300);
+  let random7 = random(20, 300);
+  let random8 = random(20, 300);
+  let random9 = random(20, 300);
+  let random10 = random(20, 300);
+  let random11 = random(20, 300);
+  let random12 = random(20, 300);
+  let random13 = random(20, 300);
+  let random14 = random(20, 300);
+  let random15 = random(20, 300);
+  let random16 = random(20, 300);
+  let random17 = random(20, 300);
+  let random18 = random(20, 300);
+  let random19 = random(20, 300);
+  let random20 = random(20, 300);
+  let randomR = random(5, 55);
+  let randomG = random(5, 55);
+  let randomB = random(5, 55);
+
+  
+  
+  fill(randomR,randomG,randomB);
+  rect(random1*-0.5, rectalheight*2, random1, rectalheight, 40);
+  fill(randomR+20,randomG+20,randomB+20);
+  rect(random2*-0.5, rectalheight*4, random2 , rectalheight, 40);
+  fill(randomR+40,randomG+40,randomB+40);
+  rect(random3*-0.5, rectalheight*6, random3 , rectalheight, 40);
+  fill(randomR+60,randomG+60,randomB+60);
+  rect(random4*-0.5, rectalheight*8, random4 , rectalheight, 40);
+  fill(randomR+80,randomG+80,randomB+80);
+  rect(random5*-0.5, rectalheight*10, random5 , rectalheight, 40);
+  fill(randomR+100,randomG+100,randomB+100);
+  rect(random6*-0.5, rectalheight*12, random6 , rectalheight, 40);
+  fill(randomR+120,randomG+120,randomB+120);
+  rect(random7*-0.5, rectalheight*14, random7 , rectalheight, 40);
+  fill(randomR+140,randomG+140,randomB+140);
+  rect(random8*-0.5, rectalheight*16, random8 , rectalheight, 40);
+  fill(randomR+160,randomG+160,randomB+160);
+  rect(random9*-0.5, rectalheight*18, random9 , rectalheight, 40);
+  fill(randomR+180,randomG+180,randomB+180);
+  rect(random10*-0.5, rectalheight*20, random10 , rectalheight, 40);
+  fill(randomR+200,randomG+200,randomB+200);
+  rect(random11*-0.5, rectalheight*22, random11 , rectalheight, 40);
+  
+}
